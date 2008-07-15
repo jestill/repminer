578d11f3bc342ef0f13827d2658fcf69ad747fcf
James Estill <JamesEstill@gmail.com>
2008-07-15 16:37:39 +0000

    Modified cnv_blast2sim.pl - Added documentation and help options

--- scripts/cnv_blast2sim.pl
+++ scripts/cnv_blast2sim.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 #-----------------------------------------------------------+
 #                                                           |
-# extract_blast.pl - Simple extract of m8 BLAST to text     |
+# cnv_blast2sim.pl - Convert BLAST report to similarity file|
 #                                                           |
 #-----------------------------------------------------------+
 #                                                           |
@@ -11,13 +11,16 @@
 # UPDATED: 07/15/2008                                       |
 #                                                           |
 # DESCRIPTION:                                              |
-# Extract blast -m8 output to sql ready format or other     |
-# general use. This also parses the RepMiner header format  |
-# and returns the unique identifier number from the fasta   |
-# header.                                                   |
+# Extract blast -m8 output to a simple three column         |
+# similarity file. This output file is a tab delimited      |
+# text file that can be easily imported into a SQL database |
+# or used as an import file for clustering.                 |
+# As part of the process this script also parses the        |
+# RepMiner header format and returns the unique identifier  |
+# number from the fasta header.                             |
 #                                                           |
-# USAGE:                                                    |
-# extract_blast.pl -i infile -o outfile.txt                 |
+# EXAMPLES:                                                 |
+# cnv_blast2sim.pl -i infile -o outfile.txt                 |
 #                                                           |
 #-----------------------------------------------------------+
 # TODO:
@@ -25,8 +28,9 @@
 #  The advantage of using a simple integer is that this 
 #  reduces the size of the file needed to store the results.
 #
+# FOR USE IN SQL DATABASES
 # FIRST:
-# extract_blast.pl my_blast_out.blo > ltr_ltr.txt
+# cnv_blast2sim.pl my_blast_out.blo > ltr_ltr.txt
 # THEN CREATE TABLE TO HOLD THIS DATA:
 # mysql> create table tbl_ltr_ltr ( 
 #                qry INT, 
@@ -54,6 +58,13 @@
 use strict;                    # 
 use Getopt::Long;              # Get options from command line
 use Bio::SearchIO;             # Parse BLAST output
+# The following needed for printing help
+use Pod::Select;               # Print subsections of POD documentation
+use Pod::Text;                 # Print POD doc as formatted text file
+use IO::Scalar;                # For print_help subfunction
+use IO::Pipe;                  # Pipe for STDIN, STDOUT for POD docs
+use File::Spec;                # Convert a relative path to an abosolute path
+
 
 #-----------------------------+
 # PROGRAM VARIABLES           |
@@ -65,8 +76,7 @@ my ($VERSION) = q$Rev$ =~ /(\d+)/;
 #-----------------------------+
 my $infile;
 my $outfile;
-
-my $blast_opt;                 # BLAST option, how to parse blast
+my $blast_opt = 2;             # BLAST option, how to parse blast
                                # Use numbers for now
                                # 1 --> Bitscore of Best HSP from -m8 BLAST
                                # 2 --> Bitscore of Tiled HSPs from -m8 BLAST
@@ -86,15 +96,13 @@ my $pre_cat_id ="0";
 #-----------------------------+
 # COMMAND LINE OPTIONS        |
 #-----------------------------+
-# Allow infile and outfile to be 
-
 #my $infile = shift;
 #my $outfile = shift;
 
 my $ok = GetOptions(# REQUIRED OPTIONS
                     "i|infile=s"    => \$infile,
                     "o|outfile=s"   => \$outfile,
-		    "b|blast-opt=s" => \$blast_opt,
+		    "b|blast-opt=s" => \$blast_opt,  # Has default
                     # ADDITIONAL OPTIONS
                     "q|quiet"       => \$quiet,
                     "verbose"       => \$verbose,
@@ -104,17 +112,49 @@ my $ok = GetOptions(# REQUIRED OPTIONS
                     "man"           => \$show_man,
                     "h|help"        => \$show_help,);
 
+
+#-----------------------------+
+# SHOW REQUESTED HELP         |
+#-----------------------------+
+if ( ($show_usage) ) {
+#    print_help ("usage", File::Spec->rel2abs($0) );
+    print_help ("usage", $0 );
+}
+
+if ( ($show_help) || (!$ok) ) {
+#    print_help ("help",  File::Spec->rel2abs($0) );
+    print_help ("help",  $0 );
+}
+
+if ($show_man) {
+    # User perldoc to generate the man documentation.
+    system ("perldoc $0");
+    exit($ok ? 0 : 2);
+}
+
+if ($show_version) {
+    print "\nbatch_mask.pl:\n".
+	"Version: $VERSION\n\n";
+    exit;
+}
+
 #-----------------------------+
 # CHECK REQUIRED VARIABLES    |
 #-----------------------------+
-if ( (!$infile) || (!$outfile) || (!$blast_opt) ) {
+if ( (!$infile) || (!$outfile) ) {
     print STDERR "\a";
     print STDERR "Infile must be specified at command line\n" if (!$infile);
     print STDERR "Outfile must be specified at command line\n" if (!$outfile);
-    print STDERR "Blast parsing option must be specified at command line\n" 
-	if (!$blast_opt);
+#    print STDERR "Blast parsing option must be specified at command line\n" 
+#	if (!$blast_opt);
     exit;
 }
+
+
+
+#-----------------------------------------------------------+
+# MAIN BODY                                                 |
+#-----------------------------------------------------------+
 
 #-----------------------------------------------------------+
 # BITSCORE OF BEST HSP : OPTION 1                           |
@@ -133,7 +173,8 @@ if ($blast_opt =~ "1" ) {
 	print STDERR "LINE: $line_num\n" if $verbose;
 	chomp;
 	
-	unless (m/^\#.*/) {       # Ignore comment lines, works with -m 9 output
+	# Ignore comment lines
+	unless (m/^\#.*/) { 
 	    
 	    my ($qry_id, $sub_id, $pid, $alen, 
 		$mismatch, $gap_open, 
@@ -175,9 +216,6 @@ elsif ( $blast_opt =~ "2" ) {
                                            '-file'   => $infile)
         || die "Could not open BLAST input file:\n$infile.\n";
 
-# The following for error reporting    
-#    if ($blast_report) { print "Blast report is valid\n"; }
-    
     while (my $blast_result = $blast_report->next_result()) {
 	
 	while (my $blast_hit = $blast_result->next_hit()) {
@@ -190,11 +228,12 @@ elsif ( $blast_opt =~ "2" ) {
             my $y_int = int($y_val);
 
 	    # PRINTING RAW NAMES
-	    #print STDOUT $blast_result->query_name."\t";  # Name of the query seq
-	    #print STDOUT $blast_hit->name."\t";           # Name of the hit seq
+	    #print STDOUT $blast_result->query_name."\t";
+	    #print STDOUT $blast_hit->name."\t";
+
 	    # PRINTING PARSED NAMES
 	    print STDOUT "$x_val\t$y_val\t";
-	    print STDOUT $blast_hit->bits()."\n";         # Tiled Bitscore of the hit
+	    print STDOUT $blast_hit->bits()."\n";
 	    
 	} # End of next BLAST hit
 	
@@ -231,11 +270,12 @@ elsif ( $blast_opt =~ "4"  ) {
             my $y_int = int($y_val);
 
 	    # PRINTING RAW NAMES
-	    #print STDOUT $blast_result->query_name."\t";  # Name of the query seq
-	    #print STDOUT $blast_hit->name."\t";           # Name of the hit seq
+	    #print STDOUT $blast_result->query_name."\t";
+	    #print STDOUT $blast_hit->name."\t";
+
 	    # PRINTING PARSED NAMES
 	    print STDOUT "$x_val\t$y_val\t";
-	    print STDOUT $blast_hit->bits()."\n";         # Tiled Bitscore of the hit
+	    print STDOUT $blast_hit->bits()."\n";
 	    
 	} # End of next BLAST hit
 
@@ -246,12 +286,296 @@ elsif ( $blast_opt =~ "4"  ) {
 }
 
 # END OF PROGRAM
-exit;
+exit 0;
+
+
+sub print_help {
+    my ($help_msg, $podfile) =  @_;
+    # help_msg is the type of help msg to use (ie. help vs. usage)
+    
+    print "\n";
+    
+    #-----------------------------+
+    # PIPE WITHIN PERL            |
+    #-----------------------------+
+    # This code made possible by:
+    # http://www.perlmonks.org/index.pl?node_id=76409
+    # Tie info developed on:
+    # http://www.perlmonks.org/index.pl?node=perltie 
+    #
+    #my $podfile = $0;
+    my $scalar = '';
+    tie *STDOUT, 'IO::Scalar', \$scalar;
+    
+    if ($help_msg =~ "usage") {
+	podselect({-sections => ["SYNOPSIS|MORE"]}, $0);
+    }
+    else {
+	podselect({-sections => ["SYNOPSIS|ARGUMENTS|OPTIONS|MORE"]}, $0);
+    }
+
+    untie *STDOUT;
+    # now $scalar contains the pod from $podfile you can see this below
+    #print $scalar;
+
+    my $pipe = IO::Pipe->new()
+	or die "failed to create pipe: $!";
+    
+    my ($pid,$fd);
+
+    if ( $pid = fork() ) { #parent
+	open(TMPSTDIN, "<&STDIN")
+	    or die "failed to dup stdin to tmp: $!";
+	$pipe->reader();
+	$fd = $pipe->fileno;
+	open(STDIN, "<&=$fd")
+	    or die "failed to dup \$fd to STDIN: $!";
+	my $pod_txt = Pod::Text->new (sentence => 0, width => 78);
+	$pod_txt->parse_from_filehandle;
+	# END AT WORK HERE
+	open(STDIN, "<&TMPSTDIN")
+	    or die "failed to restore dup'ed stdin: $!";
+    }
+    else { #child
+	$pipe->writer();
+	$pipe->print($scalar);
+	$pipe->close();	
+	exit 0;
+    }
+    
+    $pipe->close();
+    close TMPSTDIN;
+
+    print "\n";
+
+    exit 0;
+   
+}
+
+__END__;
+
+
+=head1 NAME
+
+cnv_blast2sim.pl - Convert BLAST report to similarity file
+
+=head1 VERSION
+
+This documentation refers to cnv_blast2sim.pl version $Rev:$
+
+=head1 SYNOPSIS
+
+=head2 Usage
+
+    cnv_blast2sim.pl -i BlastOutput.bln -o SimFile.txt
+
+=head2 Required Arguments
+
+    -i, --infile    # Path to the input file to parse
+    -o, --outfile   # Path to the output similarity file
+
+=head1 DESCRIPTION
+
+Extract blast -m8 or -m9 output to a simple three column
+similarity file. This output file is a tab delimited
+text file that can be easily imported into a SQL database
+or used as an import file for clustering.
+As part of the process this script also parses the
+RepMiner header format and returns the unique identifier
+number from the fasta header.
+
+=head1 REQUIRED ARGUMENTS
+
+=over 2
+
+=item -i,--infile
+
+Path of the BLAST file to be parsed.
+
+=item -o,--outfile
+
+Path of the output file.
+
+=item -b, --blast-opt
+
+Number indicating the option used to extract a similarity
+score from a BLAST result. Valid arguments are:
+
+=over
+
+=item 1.
+
+Use the BITSCORE of the best HSP.
+
+=item 2.
+
+Use a tiled BITSCORE value. This is the default option.
+
+=item 3.
+
+Use the percent id of the best HSP. THIS OPTION IS NOT CURRENTLY
+IMPLEMENTED.
+
+=item 4.
+
+Use a tiled percent id value.
+
+=back
+
+=back
+
+=head1 OPTIONS
+
+=over 2
+
+=item --logfile
+
+Path to a file that will be used to log program status.
+If the file already exists, additional information will be concatenated
+to the existing file.
+
+=item --usage
+
+Short overview of how to use program from command line.
+
+=item --help
+
+Show program usage with summary of options.
+
+=item --version
+
+Show program version.
+
+=item --man
+
+Show the full program manual. This uses the perldoc command to print the 
+POD documentation for the program.
+
+=item --verbose
+
+Run the program with maximum output.
+
+=item -q,--quiet
+
+Run the program with minimal output.
+
+=item --test
+
+Run the program without doing the system commands.
+
+=back
+
+=head1 DIAGNOSTICS
+
+Error messages generated by this program and possible solutions are listed
+below.
+
+=over 2
+
+=item ERROR: Could not create the output directory
+
+The output directory could not be created at the path you specified. 
+This could be do to the fact that the directory that you are trying
+to place your base directory in does not exist, or because you do not
+have write permission to the directory you want to place your file in.
+
+=back
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+This script does not make use of external configuration files
+or any options in the user environment.
+
+=head1 DEPENDENCIES
+
+=head2 Required Software
+
+=over
+
+=item * NCBI blastall
+
+The latest version of the NCBI blastall program can be downloaded from:
+ftp://ftp.ncbi.nih.gov/blast/executables/LATEST
+
+=back
+
+=head2 Required Perl Modules
+
+=over
+
+=item * Bio::SearchIO
+
+This module is part of bioperl and is required to parse BLAST
+output in a format that is tiled across all HSPs.
+
+=item * Getopt::Long
+
+This module is required to accept options at the command line.
+
+=back
+
+=head1 BUGS AND LIMITATIONS
+
+=head2 Bugs
+
+=over 2
+
+=item * No bugs currently known 
+
+If you find a bug with this software, file a bug report on the DAWG-PAWS
+Sourceforge website: http://sourceforge.net/tracker/?group_id=192812
+
+=back
+
+=head2 Limitations
+
+=over
+
+=item * Limited to m8 or m9 BLAST format
+
+This script is designed to be a lean a fast parser of the 
+similarity information from BLAST. It is therefore limited
+to using the simple m8 or m9 BLAST alignment format.
+
+=back
+
+=head1 SEE ALSO
+
+The cnv_blast2sim.pl program is part of the repminer package of 
+repeat element annotation programs.
+See the DAWG-PAWS web page 
+( http://repminer.sourceforge.net/ )
+or the Sourceforge project page 
+( http://sourceforge.net/projects/repminer/ )
+for additional information about this package.
+
+=head1 LICENSE
+
+GNU GENERAL PUBLIC LICENSE, VERSION 3
+
+http://www.gnu.org/licenses/gpl.html
+
+THIS SOFTWARE COMES AS IS, WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTY. USE AT YOUR OWN RISK.
+
+=head1 AUTHOR
+
+James C. Estill E<lt>JamesEstill at gmail.comE<gt>
+
+=head1 HISTORY
+
+STARTED: 02/24/2008
+
+UPDATED: 07/15/2008
+
+VERSION: $Rev:$
+
+=cut
+
 
 #-----------------------------------------------------------+
-# HISTORY                                                   |
+# CHANGELOG                                                 |
 #-----------------------------------------------------------+
-# 
 #
 # 03/09/2008
 # - Added Getopt long 
@@ -264,6 +588,10 @@ exit;
 #    4 --> PID of Tiled HSPs
 # 
 # 07/15/2008
+# - Renamed from extract_blast.pl to cnv_blast2sim.pl
+#   this name reflects the fact this this converts native
+#   m8 BLAST output to a simple three column similarity
+#   file.
 # - Adding option for input from STDIN
 
 # TO DO:
